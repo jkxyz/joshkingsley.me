9df86fb3f85bbb5fc037c1d7b749e7a4858ffa8b
Josh Kingsley <josh@joshkingsley.me>
2025-03-19 13:32:36 +0200

    Improve dev server

--- site/server.js
+++ site/server.js
@@ -1,33 +1,42 @@
-const nodeStatic = require('node-static');
-const http = require('http');
-const chokidar = require('chokidar');
+const http = require("http");
+const serveStatic = require("serve-static");
+const finalhandler = require("finalhandler");
+const chokidar = require("chokidar");
 
-function build () {
-  delete require.cache[require.resolve('./config')];
-  const config = require('./config');
-  config.build((err) => {
-    if (err) {
-      throw err;
-    }
+function build() {
+  return new Promise((resolve, reject) => {
+    console.log("Building...");
+
+    delete require.cache[require.resolve("./config")];
+
+    const config = require("./config");
+
+    config.build((err) => {
+      if (err) return reject(err);
+      console.log("Done");
+      resolve();
+    });
   });
 }
 
-build();
-
 chokidar
-  .watch(['config.js', 'layouts/**/*', 'src/**/*'], { ignoreInitial: true })
-  .on('all', () => {
-    build();
+  .watch(["config.js", "layouts/**/*", "src/**/*"], { ignoreInitial: true })
+  .on("all", () => {
+    build().catch((err) => {
+      console.error("Error during build:", err);
+    });
   });
 
-const fileServer = new nodeStatic.Server('target', { cache: false });
+const staticHandler = serveStatic("target");
 
-http
-  .createServer((req, res) => {
-    req
-      .addListener('end', () => { fileServer.serve(req, res); })
-      .resume();
-  })
-  .listen(3000, () => {
-    console.log('Listening on port 3000');
+const server = http.createServer((req, res) => {
+  staticHandler(req, res, finalhandler(req, res));
+});
+
+const port = process.env.PORT || 3000;
+
+build().then(() => {
+  server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
   });
+});
